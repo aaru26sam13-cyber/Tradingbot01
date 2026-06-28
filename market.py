34e7a4b4db9f355860393975cf8d42ed3749fe25
aaru26sam13-cyber <aaru26sam13@gmail.com>
2026-06-29 02:41:24 +0530

    Refactor get_price to get_crypto_price function

--- market.py
+++ market.py
@@ -1,27 +1,6 @@
 import requests
 
-def get_price(symbol):
-    symbol = symbol.upper().strip()   # मोठे अक्षर + space remove
-
-    # 🔥 जर USDT नसेल तर auto add कर
-    if "USDT" not in symbol:
-        symbol = symbol + "USDT"
-
+def get_crypto_price(symbol):
     url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-
-    try:
-        response = requests.get(url, timeout=10)
-        data = response.json()
-
-        # ✅ price आला तर ok
-        if "price" in data:
-            return float(data["price"])
-
-        # ❌ error आला तर print कर
-        else:
-            print("API Error:", data)
-            return None
-
-    except Exception as e:
-        print("Error:", e)
-        return None
+    r = requests.get(url).json()
+    return float(r["price"]) if "price" in r else None
